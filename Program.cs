553267d331d6613cb20dfa1c618d53c2957c4eff
Vivian Pinheiro <vivibp@bol.com.br>
2024-01-09 12:28:17 -0300

    Desafio 2 concluído com sucesso!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Desafio 2 concluído com sucesso.
+
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia(numero: "81125776", modelo: "A", imei: "123ABC", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("IPhone:");
+Smartphone iphone = new Iphone(numero: "86110560", modelo: "B", imei: "123XYZ", memoria: 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
